b8d940eaa8d34048f102f3f486e836e9b457ad8b
elisafvic <elisapersea@gmail.com>
2023-05-04 20:54:45 +0200

    2nd and 3rd tests: does not validate code that ends with < nor code with different number of < and >

--- CodeLinter.rb
+++ CodeLinter.rb
@@ -5,12 +5,37 @@ class CodeLinter
     end
 
     def validate
-        return validOpening
+        return validOpening && validClosing && sameNumberOfSymbols
     end
 
     private
 
     def validOpening
         @code[0] == '<'
+    end
+
+    def validClosing
+        @code[-1] == '>'
+    end
+
+    def sameNumberOfSymbols
+        openings = 0
+        
+        closings = 0
+        parseableCode = @code.split('')
+
+        parseableCode.each do |char|
+            if char == '<'
+                openings += 1
+            end
+            if char == '>'
+                closings += 1
+            end
+        end
+        p parseableCode
+        p "Number of openings: #{openings}"
+        p "Number of closings: #{closings}"
+        
+        return openings == closings
     end
 end
--- spec/CodeLinter_spec.rb
+++ spec/CodeLinter_spec.rb
@@ -3,7 +3,23 @@ require_relative '../CodeLinter.rb'
 RSpec.describe CodeLinter do
 
   it 'does not validate code that starts with >' do
-    test_code = CodeLinter.new('>')
+    test_code = CodeLinter.new('><>')
+
+    is_valid = test_code.validate
+
+    expect(is_valid).to eq(false)
+  end
+
+  it 'does not validate code that ends with <' do
+    test_code = CodeLinter.new('<>><')
+
+    is_valid = test_code.validate
+
+    expect(is_valid).to eq(false)
+  end
+
+  it 'does not validate code if number of "<" and ">" is not the same' do
+    test_code = CodeLinter.new('<>><>')
 
     is_valid = test_code.validate
 
