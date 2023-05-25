0248d79559e185cbadf7657fa3c2c1275e1b84d3
Elisa <elisa@flexmyroom.com>
2023-05-25 09:39:25 +0200

    Naming change: variable changes to program to avoid confusion with class

--- Linter.rb
+++ Linter.rb
@@ -4,43 +4,42 @@ require_relative 'Code.rb'
 
 class Linter
 
-    def initialize(code)
-        @code = code
+    def initialize(program)
+        @program = program
     end
 
     def validate
-        if Character.incompatible_characters?(@code)
+        if Character.incompatible_characters?(@program)
             return false
         end
 
-        parseable_code = CodeManipulator.convert_code(@code)
+        parseable_code = CodeManipulator.convert_code(@program)
 
         matched_pairs?(parseable_code)
     end
 
     private
 
-
-    def matched_pairs?(code)
-        if Character.is_closing?(code.first)
+    def matched_pairs?(program)
+        if Character.is_closing?(program.first)
             return false
         else
-            closing = Character.matching_closing(code.first)
+            closing = Character.matching_closing(program.first)
 
-            if Code.has_no_char?(code, closing)
+            if Code.has_no_char?(program, closing)
                 return false
             end
 
-            code.each do |char|
+            program.each do |char|
                 if char == closing
-                    code = CodeManipulator.delete_valid_pair(code, closing)
+                    program = CodeManipulator.delete_valid_pair(program, closing)
 
-                    if Code.no_chars_left?(code)
+                    if Code.no_chars_left?(program)
                         return true
-                    elsif Code.one_char_left?(code)
+                    elsif Code.one_char_left?(program)
                         return false
-                    elsif Code.two_or_more_chars_left?(code)
-                        return matched_pairs?(code)
+                    elsif Code.two_or_more_chars_left?(program)
+                        return matched_pairs?(program)
                     end
                 end
             end
